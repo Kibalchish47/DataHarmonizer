45c81244c63c961022af3ca0377514b84df51eba
Kibalchish47 <kibalchishthecoder@gmail.com>
2024-03-17 18:56:56 +0200

    Convert Excel to CSV Function

--- DataHarmonizer.py
+++ DataHarmonizer.py
@@ -2,3 +2,15 @@
 import os
 import pandas as pd
 
+def convert_excel_to_csv(input_folder, output_folder):
+    
+    os.makedirs(output_folder, exist_ok=True)
+
+    for filename in os.listdir(input_folder):
+        if filename.endswith(".xlsx"):
+            excel_path = os.path.join(input_folder, filename)
+            csv_path = os.path.join(output_folder, filename.replace(".xlsx", ".csv"))
+
+            df = pd.read_excel(excel_path)
+
+            df.to_csv(csv_path, index=False)
